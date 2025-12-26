c0d41eae365fb7ef11adef8e969696df371e734e
itscrowley <45391180+itscrowley@users.noreply.github.com>
2025-12-26 21:34:14 +0530

    ProductCard.tsx

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,9 +1,18 @@
 import { isOutOfStock } from "@/lib/stock";
+import type { Product } from "@/models/Product";
 
-export default function ProductCard({ product }) {
+type Props = {
+  product: Product;
+};
+
+export default function ProductCard({ product }: Props) {
   return (
     <div className="relative">
-      <img src={product.images[0]} className="aspect-square object-cover" />
+      <img
+        src={product.images[0]}
+        className="aspect-square object-cover rounded"
+        alt={product.name}
+      />
 
       {isOutOfStock(product.stock) && (
         <span className="absolute top-2 left-2 bg-red-600 text-white px-2 text-xs rounded">
